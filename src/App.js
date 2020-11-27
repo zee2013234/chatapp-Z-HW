b9fa9160fe98654ce12547f2d158d55d1e7343c6
Parkhyunwoo1515 <74614792+Parkhyunwoo1515@users.noreply.github.com>
2020-11-27 14:43:54 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './Pages/HomePage.js';
 import NextPage from './Pages/NextPage.js';
+import DiscussPage from './Pages/Discuss.js'
+
 class App extends Component {
     render() {
         return (
@@ -9,6 +11,7 @@ class App extends Component {
                 <Switch>
                     <Route path="/" component={HomePage} />
                     <Route path="/writingroom" component={NextPage} />
+                    <Route path="/dicuss" component={DiscussPage}/>
                 </Switch>
             </div>
         );
