939a7a4eecf106b550ab9aea57a65bbed1a36e47
Parkhyunwoo1515 <74614792+Parkhyunwoo1515@users.noreply.github.com>
2020-11-27 14:44:51 +0900

    Create Discuss.js

---
+++ src/Pages/Discuss.js
@@ -0,0 +1,16 @@
+import React, { Component, useRef, useState } from 'react';
+
+import Discuss from './Discuss.js';
+
+
+function AAA() {
+    return (<>
+
+        <main>
+          AaA
+        </main>
+    </>)
+}
+
+
+export default AAA;
