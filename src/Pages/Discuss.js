1765acd28afc4a4b8f86b2638bf87b8602b861f9
Parkhyunwoo1515 <74614792+Parkhyunwoo1515@users.noreply.github.com>
2020-11-27 15:44:28 +0900

    Update Discuss.js

--- src/Pages/Discuss.js
+++ src/Pages/Discuss.js
@@ -1,5 +1,11 @@
 import React from 'react';
-
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import 'firebase/analytics';
+import Header from './Header.js'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
 import Discuss from './Discuss.js';
 import { Link, Route } from 'react-router-dom';
 
@@ -7,11 +13,7 @@ import { Link, Route } from 'react-router-dom';
 
 function AAA() {
     return (<>
-        <button className="go-back"><Link to="/">Go Back></Link></button>
-
-        <main>
-          AaA
-        </main>
+        <Header/>
     </>)
 }
 
