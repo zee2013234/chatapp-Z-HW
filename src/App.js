8cedc6c0288dd068eb4bb7e5435e58b07be59acc
zee2013234 <zee2013234@kaist.ac.kr>
2020-11-27 13:02:49 +0900

    roomlist implemented

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import HomePage from './Pages/HomePage.js';
 import NextPage from './Pages/NextPage.js';
+import Head from './Pages/Head.js';
 class App extends Component {
     render() {
         return (
@@ -9,6 +10,8 @@ class App extends Component {
                 <Switch>
                     <Route path="/" component={HomePage} />
                     <Route path="/writingroom" component={NextPage} />
+                    <Route path="/main" component={Head}/>
+
                 </Switch>
             </div>
         );
---
+++ src/Pages/Head.js
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+import { Link, Route } from 'react-router-dom';
+
+
+
+const Head = () => {
+
+  return (
+    <div className="HD-header">
+     This is a test page
+    </div>
+  )
+};
+
+export default Head;
