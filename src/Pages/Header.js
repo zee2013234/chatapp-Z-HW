6517aa632e14af4b87749871a4ed5e3ae8653ed6
Parkhyunwoo1515 <74614792+Parkhyunwoo1515@users.noreply.github.com>
2020-11-28 13:40:45 +0900

    Update Header.js

--- src/Pages/Header.js
+++ src/Pages/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
+import DiscussPage from './Discuss.js';
 import '../style/Header.css'
 import firebase from 'firebase/app';
 import 'firebase/firestore';
@@ -12,15 +13,15 @@ const Header = () => {
   return (
     <div className="HD-header">
       <ul className="HD-navbar">
-        <li><Link to="/library" className="Hd-link HD-logo">롤링롤링</Link></li>
-        <li><Link to="/library" className="Hd-link">소설 작성방</Link></li>
-        <li><Link to="/library" className="Hd-link HD-la">완결 작품들</Link></li>
+        <li><Link to="/" className="Hd-link HD-logo">롤링롤링</Link></li>
+        <li><Link to="/" className="Hd-link">소설 작성방</Link></li>
+        <li><Link to="/" className="Hd-link HD-la">완결 작품들</Link></li>
         <div className="HD-rightones">
-        <li><Link to="/mypage" className="Hd-link">마이페이지</Link></li>
-        <li><Link to="/writingroom" className="Hd-link">알림</Link></li>
-        <li><Link to="/discuss" className="Hd-link">설정</Link></li>
+        <li><Link to="/" className="Hd-link">마이페이지</Link></li>
+        <li><Link to="/" className="Hd-link">알림</Link></li>
+        <li><Link to="/" className="Hd-link">설정</Link></li>
         <SignOut />
-        </div>
+      </div>
       </ul>
       <hr></hr>
 
